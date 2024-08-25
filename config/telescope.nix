3d17c467241ce93b6a30039436e29fbc9e4bd61d
KurzF <kurzr.f@gmail.com>
2024-08-25 23:45:36 +0200

    add keybind for find_files

--- config/telescope.nix
+++ config/telescope.nix
@@ -9,5 +9,10 @@
 	    key = "<space>g";
 	    action = "<cmd>Telescope live_grep<cr>";
 	}
+	{
+	    mode = [ "n" ];
+	    key = "<space>p";
+	    action = "<cmd>Telescope find_files<cr>";
+	}
     ];
 }
